12d6e5c132c35d308d0a75adbe2efa7273b45c44
RMYanenko <yanenko.r.m@gmail.com>
2020-08-08 14:56:35 +0300

    add Ckass

--- lib/main.dart
+++ lib/main.dart
@@ -36,11 +36,23 @@ class _QuizPageState extends State<QuizPage> {
     ),
   ];
 
-  List<String>quarter = [
+  List<String> quarter = [
     'You can lead a cow down stairs but not up stairs.',
     'Approximately one quarter of human bones are in the feet.',
     'A slug\'s blood is green.',
   ];
+
+  List<bool> answers = [
+    false,
+    true,
+    true,
+  ];
+
+  int questionNumder = 0;
+
+  // int nextQuest(questionNumder){
+  //   print(questionNumder + 1);
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +66,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quarter[questionNumder],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -79,13 +91,19 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool correctAnswer = answers[questionNumder];
+                if (correctAnswer == true) {
+                  print('User got it right!');
+                } else {
+                  print('User got it wrong!');
+                }
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    )
-                  );
+                  scoreKeeper.add(Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ));
+                  // nextQuest()
+                  questionNumder++;
                 });
               },
             ),
@@ -105,13 +123,19 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+
+                bool correctAnswer = answers[questionNumder];
+                if (correctAnswer == false) {
+                  print('User got it right!');
+                } else {
+                  print('User got it wrong!');
+                }
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    )
-                  );
+                  scoreKeeper.add(Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  ));
+                  questionNumder++;
                 });
               },
             ),
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questionText;
+  bool quarterAnswer;
+
+  Question({String q, bool a}) {
+    questionText = q;
+    quarterAnswer = a;
+  }
+}
